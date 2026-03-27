78c5cebc43d4baf6c665a811d62eb40e997ad1de
gowthams-cdw <gowtham.s@cdw.com>
2026-03-27 10:15:12 +0530

    Fix: variable names

--- basic-js/q2.js
+++ basic-js/q2.js
@@ -12,34 +12,39 @@ const indiaSquad2026 = [
 	"Arshdeep Singh",
 	"Varun Chakaravarthy",
 ];
-console.log("Initial 11: " + JSON.stringify(indiaSquad2026));
+console.log(`Initial 11: ${JSON.stringify(indiaSquad2026)}`);
 
 // Unfortunately, the first player had an injury. Remove him from the list of players.
-indiaSquad2026.shift();
+const removedPlayer = indiaSquad2026.shift();
 console.log(
-	"Initial 11(After removal of Rinku): " + JSON.stringify(indiaSquad2026),
+	`Playing 11 (After removal of ${removedPlayer}): ${JSON.stringify(indiaSquad2026)}`,
 );
 
 // Now, find out the number of players
 console.log("Length of team: " + indiaSquad2026.length);
 
 // Add another player to the above list of players to make the count 11.
-indiaSquad2026.unshift("Sanju Samson");
-console.log("Modified 11: " + JSON.stringify(indiaSquad2026));
+const newPlayer = "Sanju Samson";
+indiaSquad2026.unshift(newPlayer);
+console.log(
+	`Modified 11 after inclusion of ${newPlayer}: ${JSON.stringify(indiaSquad2026)}`,
+);
 
 // The cricket board has decided to take photographs of all players and so they would need the players list in sorted format.
 indiaSquad2026.sort();
-console.log("Sorted 11: " + JSON.stringify(indiaSquad2026));
+console.log(`Sorted 11: ${JSON.stringify(indiaSquad2026)}`);
 
 // Display all the Players name and assign a random jersey number. For example. MS Dhoni-7
-const squadWithRandomJersyNo = indiaSquad2026.reduce((acc, member) => {
-	acc[member] = Math.round(Math.random() * 1000);
-	return acc;
-}, {});
+const squadWithRandomJersyNumber = indiaSquad2026.map((member) => {
+	const jerseyNumber = Math.round(Math.random() * 1000);
+	return `${member} - ${jerseyNumber}`;
+});
 console.log(
-	"Squad members with jersy no: " + JSON.stringify(squadWithRandomJersyNo),
+	"Squad members with jersy no: " + JSON.stringify(squadWithRandomJersyNumber),
 );
 
 // The cricket board wants to print the names of all players in uppercase and store it in a different location for printing jerseys. Do not modify the existing players list.
-const uppercaseMemberNames = indiaSquad2026.map((member) => member.toUpperCase())
+const uppercaseMemberNames = indiaSquad2026.map((member) =>
+	member.toUpperCase(),
+);
 console.log("11 with uppercase name: " + JSON.stringify(uppercaseMemberNames));
--- basic-js/q3.js
+++ basic-js/q3.js
@@ -4,7 +4,6 @@ const displayFromOneToHundred = () => {
 		console.log(i);
 	}
 };
-
 displayFromOneToHundred();
 
 // Create a function to display today's date in DD/MM/YYYY format
@@ -20,37 +19,27 @@ const printTodayDate = () => {
 printTodayDate();
 
 // Create a function which accepts a Celsius value as parameter and returns the Fahrenheit value
-const celsiusToFahrenheit = (celsius) => {
+const convertFromCelsiusToFahrenheit = (celsius) => {
 	return (celsius * 9) / 5 + 32;
 };
+const celsiusValue = 25;
 console.log(
-	`The Fahrenheit value of 25 degree celsius: ${celsiusToFahrenheit(25)}`,
+	`The Fahrenheit value of ${celsiusValue} degree celsius: ${convertFromCelsiusToFahrenheit(celsiusValue)}`,
 );
 
 // Create a function which accepts an array of numbers as parameter and return the average of those numbers.
-const getAvg = (array) => {
-	const sum = array.reduce((acc, curSum) => acc + curSum, 0);
-	return sum / array.length;
+const getAverage = (array) => {
+	const arraySum = array.reduce((accSum, curSum) => accSum + curSum, 0);
+	return arraySum / array.length;
 };
+const arrayToCalculateAverage = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(
-	`The avg of array [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]: ${getAvg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])}`,
+	`The avg of array ${arrayToCalculateAverage}: ${getAverage(arrayToCalculateAverage)}`,
 );
 
 // Create a function to reverse a given string.
-const getRevString = (v) => {
-	const charArray = v.split("");
-
-	let i = 0,
-		j = charArray.length - 1,
-		temp;
-	while (i < j) {
-		temp = charArray[i];
-		charArray[i] = charArray[j];
-		charArray[j] = temp;
-		i++;
-		j--;
-	}
-
-	return charArray.join("");
-};
-console.log(`The reverse word of alpha: ${getRevString("alpha")}`);
+const getReverseOfString = (v) => v.split("").reverse().join("");
+const stringToReverse = "wake up, neo";
+console.log(
+	`The reverse of word ${stringToReverse}: ${getReverseOfString(stringToReverse)}`,
+);
