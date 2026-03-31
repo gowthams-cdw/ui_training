c87aebcb772b2bce5dda13c4654345dae0cc895d
gowthams-cdw <gowtham.s@cdw.com>
2026-03-31 11:39:47 +0530

    Fix: functionalities

--- friends/index.js
+++ friends/index.js
@@ -1,3 +1,4 @@
+// function that takes a json filename as input, and parses the json and return it
 const parseJsonFile = async (fileName) => {
 	try {
 		const response = await fetch(fileName);
@@ -11,28 +12,45 @@ const parseJsonFile = async (fileName) => {
 	}
 };
 
-const loadInitialData = async () => {
+// function that loads the initial data from the friends.json file and renders it to the page
+const loadFriendsData = async () => {
 	const friendsJson = await parseJsonFile("data/friends.json");
-	console.log(friendsJson);
 
-	const mainContainer = document.getElementsByClassName("main")[0];
-	mainContainer.innerHTML = friendsJson
-		.map((friend) => {
-			return `
-		<article class="card">
-			<figure class="card__img-container">
-				<img src="${friend.img}" alt="profile" class="card__img" loading="lazy" />
-			</figure>
-			<div class="card__content">
-				<span class="card__content-name">
-					${friend.first_name} ${friend.last_name}
-				</span>
-				<span class="card__content-email">${friend.email}</span>
-			</div>
-		</article>
-		`;
-		})
-		.join("");
+	const cardsContainer = document.getElementById("cards-container");
+	friendsJson.forEach((friend) => {
+		const article = document.createElement("article");
+		article.className = "card";
+
+		const figure = document.createElement("figure");
+		figure.className = "card__img-container";
+
+		const img = document.createElement("img");
+		img.src = friend.img;
+		img.alt = "profile";
+		img.className = "card__img";
+		img.loading = "lazy";
+		figure.appendChild(img);
+
+		const contentDiv = document.createElement("div");
+		contentDiv.className = "card__content";
+
+		const nameSpan = document.createElement("span");
+		nameSpan.className = "card__content-name";
+		nameSpan.textContent = `${friend.first_name} ${friend.last_name}`;
+
+		const emailSpan = document.createElement("a");
+		emailSpan.className = "card__content-email";
+		emailSpan.textContent = friend.email;
+		emailSpan.href = `mailto:${friend.email}`;
+
+		contentDiv.appendChild(nameSpan);
+		contentDiv.appendChild(emailSpan);
+
+		article.appendChild(figure);
+		article.appendChild(contentDiv);
+
+		cardsContainer.appendChild(article);
+	});
 };
 
-loadInitialData();
+loadFriendsData();
